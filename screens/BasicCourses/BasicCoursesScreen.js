425401948d5dd9f21687414b1035755de8017e77
ipin0917826334 <71114672+ipin0917826334@users.noreply.github.com>
2022-11-17 18:20:09 +0700

    Filter Basic Course and Add Bmi in Drawer
    Filter Basic Course and Add Bmi in Drawer

--- screens/BasicCourses/BasicCoursesScreen.js
+++ screens/BasicCourses/BasicCoursesScreen.js
@@ -1,165 +1,152 @@
 import { StyleSheet, Text, View, FlatList, TouchableOpacity, Image, TextInput } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { db } from '../../database/firebase';
-import { collection, onSnapshot, orderBy, query} from 'firebase/firestore';
-import React, {useState, useEffect} from 'react';
-const BasicCoursesScreen = ({navigation})  => {
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import React, { useState, useEffect } from 'react';
+const BasicCoursesScreen = ({ navigation }) => {
   const [courseData, setCourseData] = useState([])
-
+  const [filterCourse, setFilterCourse] = useState([])
   const [hasfilter, setFilter] = useState(false)
   const [age, setAge] = useState('')
-  const [height, setHeight] = useState('')
-  const [weight, setWeight] = useState('')
   useEffect(() => {
     const q = query(collection(db, 'defaultCourse'), orderBy("courseId", 'asc'))
     onSnapshot(q, (snapshot) => {
-        setCourseData(snapshot.docs.map(doc => doc.data()))
+      setCourseData(snapshot.docs.map(doc => doc.data()))
     })
   }, [])
   const formatTime = (time) => {
-    return Math.floor(time/60) + " นาที " + time%60 +  " วินาที"
+    return Math.floor(time / 60) + " นาที " + time % 60 + " วินาที"
   }
   const filterData = () => {
-    setFilter(false)
+    setFilter(true)
+    setFilterCourse(courseData);
+    if (age > 0 && age <= 13) {
+      setFilterCourse(courseData.filter(ages => ages.minAge >= 0 && ages.maxAge <= 13))
+    }
+    else if (age >= 14 && age <= 25) {
+      setFilterCourse(courseData.filter(ages => ages.minAge >= 14 && ages.maxAge <= 25))
+    }
+    else if (age >= 26 && age <= 59) {
+      setFilterCourse(courseData.filter(ages => ages.minAge >= 26 && ages.maxAge <= 59))
+    }
+    else if (age >= 60 && age <= 120) {
+      setFilterCourse(courseData.filter(ages => ages.minAge >= 60 && ages.maxAge <= 120))
+    }
+    else{
+      setFilterCourse(courseData);
+    }
+
 
   }
-  const renderItem = ({item, index}) => {
-    return(
-      <View key={index} style={{height : 370, 
-        width : '100%',
-        borderWidth : 1, 
-        borderColor : 'lightgray', 
-        borderRadius : 5,
-        marginBottom : 15,
-        backgroundColor : '#F2F2F2'}}>      
+  const renderItem = ({ item, index }) => {
+    return (
+      <View key={index} style={{
+        height: 370,
+        width: '100%',
+        borderWidth: 1,
+        borderColor: 'lightgray',
+        borderRadius: 5,
+        marginBottom: 15,
+        backgroundColor: '#F2F2F2'
+      }}>
         <View style={[styles.gridItem]}>
-        <Image source={{ uri: item.image }} style={[styles.bgImage]}/>
+          <Image source={{ uri: item.image }} style={[styles.bgImage]} />
         </View>
 
-        <View style={{position : 'absolute', width : '95%', top : 160, paddingTop : 10, paddingRight : 10, paddingLeft : 10}}>
+        <View style={{ position: 'absolute', width: '95%', top: 160, paddingTop: 10, paddingRight: 10, paddingLeft: 10 }}>
 
-        <Text style={{marginLeft : 10, fontSize : 16, fontWeight : '700'}}>
-          {item.courseTitle}
-        </Text>
+          <Text style={{ marginLeft: 10, fontSize: 16, fontWeight: '700' }}>
+            {item.courseTitle}
+          </Text>
 
-        <Text style={{marginLeft : 10, marginTop : 10}}>
-          {"    "}{item.desc}
-        </Text>
+          <Text style={{ marginLeft: 10, marginTop: 10 }}>
+            {"    "}{item.desc}
+          </Text>
 
-        <Text style={{marginLeft : 10, marginTop : 10}}>
-          ท่าออกกำลังกาย <Text style={{fontWeight : '700'}}>{item.postureData.length}</Text>
-        </Text>
-        <Text style={{marginLeft : 10, marginTop : 10}}>
-          เวลารวม{" "}
-        <Text style={{fontWeight : '700'}}>{formatTime(item.postureData.map(data => data.timeDuration).reduce((prev, curr) => (prev+15) + curr))}</Text>
-        </Text>
+          <Text style={{ marginLeft: 10, marginTop: 10 }}>
+            ท่าออกกำลังกาย <Text style={{ fontWeight: '700' }}>{item.postureData.length}</Text>
+          </Text>
+          <Text style={{ marginLeft: 10, marginTop: 10 }}>
+            เวลารวม{" "}
+            <Text style={{ fontWeight: '700' }}>{formatTime(item.postureData.map(data => data.timeDuration).reduce((prev, curr) => (prev + 15) + curr))}</Text>
+          </Text>
 
         </View>
 
-        <TouchableOpacity style={{width : '100%', 
-                            height : 40, 
-                            backgroundColor : 'lightblue',
-                            alignItems : 'center',
-                            justifyContent : 'center',
-                            borderRadius : 5}}
-        onPress={() => {
-        navigation.navigate("Course Detail", {courseTitle : item.courseTitle, courseData : item})
-        }}>
-        <Ionicons name='ios-enter-outline' size={20} color={'black'}/>
+        <TouchableOpacity style={{
+          width: '100%',
+          height: 40,
+          backgroundColor: 'lightblue',
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderRadius: 5
+        }}
+          onPress={() => {
+            navigation.navigate("Course Detail", { courseTitle: item.courseTitle, courseData: item })
+          }}>
+          <Ionicons name='ios-enter-outline' size={20} color={'black'} />
         </TouchableOpacity>
-        </View>
-      )
+      </View>
+    )
   }
   return (
     <View style={styles.container}>
-        {
-          !hasfilter ?
+      {
+        !hasfilter ?
           <TouchableOpacity style={{
-            width : '95%', height : 40, backgroundColor : '#ff872b',
-            borderRadius : 5, alignItems : 'center', justifyContent : 'center', marginTop : 10
-          }} onPress={() => {setFilter(!hasfilter)}}>
-            <Text style={{color : 'white', fontWeight : '600', fontSize : 18}}>กรองคอร์ส</Text>
-         </TouchableOpacity>
-         :
-         null
-        }
-      {hasfilter ? 
+            width: '95%', height: 40, backgroundColor: '#ff872b',
+            borderRadius: 5, alignItems: 'center', justifyContent: 'center', marginTop: 10
+          }} onPress={() => { setFilter(!hasfilter) }}>
+            <Text style={{ color: 'white', fontWeight: '600', fontSize: 18 }}>กรองคอร์ส</Text>
+          </TouchableOpacity>
+          :
+          null
+      }
+      {hasfilter ?
         <View style={{
-          width : '95%',
-          height : 'auto',
-          backgroundColor : 'white',
+          width: '95%',
+          height: 'auto',
+          backgroundColor: 'white',
           shadowColor: '#171717',
-          shadowOffset: {width: -2, height: 4},
+          shadowOffset: { width: -2, height: 4 },
           shadowOpacity: 0.2,
           shadowRadius: 3,
-          borderRadius : 10,
-          marginTop : 10, padding : 10
+          borderRadius: 10,
+          marginTop: 10, padding: 10
         }}>
-          <View style={{width : '100%'}}>
-            <Text style={{marginBottom : 15, fontSize : 24, fontWeight  : '700'}}>แทบกรอง</Text>
-            <TouchableOpacity onPress={() => {setFilter(false)}} style={{position : 'absolute', right : 0}}>
+          <View style={{ width: '100%' }}>
+            <Text style={{ marginBottom: 15, fontSize: 24, fontWeight: '700' }}>แทบกรอง</Text>
+            <TouchableOpacity onPress={() => { setFilter(false) }} style={{ position: 'absolute', right: 0 }}>
               <Ionicons name='ios-close-outline' size={25} color={'black'} />
             </TouchableOpacity>
           </View>
-          <View style={{flexDirection : 'column'}}>
-            <Text style={{marginBottom : 5}}>อายุ</Text>
+          <View style={{ flexDirection: 'column' }}>
+            <Text style={{ marginBottom: 5 }}>อายุ</Text>
             <TextInput style={{
-              width : '40%',
-              height : 40,
-              borderWidth : 1,
-              borderColor : 'lightgray',
-              borderRadius : 5, padding : 5
+              width: '40%',
+              height: 40,
+              borderWidth: 1,
+              borderColor: 'lightgray',
+              borderRadius: 5, padding: 5
             }}
-            placeholder="ใส่อายุของคุณ"
-            keyboardType='numeric'
-            onChangeText={setAge}
-            value={age}
+              placeholder="ใส่อายุของคุณ"
+              keyboardType='numeric'
+              onChangeText={setAge}
+              value={age}
             />
           </View>
-          <View style={{flexDirection : 'row', marginTop : 10}}>
-            <View style={{width : '40%', marginRight : 10}}>
-              <Text style={{marginBottom : 5}}>ส่วนสูง</Text>
-              <TextInput style={{
-                width : '100%',
-                height : 40,
-                borderWidth : 1,
-                borderColor : 'lightgray',
-                borderRadius : 5, padding : 5
-              }}
-              placeholder="ส่วนสูง"
-              keyboardType='numeric'
-              onChangeText={setHeight}
-              value={height}
-              />
-            </View>
-            <View style={{width : '40%'}}>
-              <Text style={{marginBottom : 5}}>น้ำหนัก</Text>
-              <TextInput style={{
-                width : '100%',
-                height : 40,
-                borderWidth : 1,
-                borderColor : 'lightgray',
-                borderRadius : 5, padding : 5
-              }}
-              placeholder="น้ำหนัก"
-              keyboardType='numeric'
-              onChangeText={setWeight}
-              value={weight}
-              />
-            </View>
-          </View>
           <TouchableOpacity style={{
-              width : '20%', height : 40, backgroundColor : 'lightblue',
-              borderRadius : 5, alignItems : 'center', justifyContent : 'center', marginTop : 10
-            }} onPress={() => {filterData()}}>
-              <Text style={{color : 'white'}}>กรอง</Text>
+            width: '20%', height: 40, backgroundColor: 'lightblue',
+            borderRadius: 5, alignItems: 'center', justifyContent: 'center', marginTop: 10
+          }} onPress={() => { filterData() }}>
+            <Text style={{ color: 'white' }}>กรอง</Text>
           </TouchableOpacity>
         </View>
         :
         null
-        }
-      <View style={[styles.shadowbox, {width : "100%", padding : 10}]}>
-        <FlatList data={courseData} renderItem={renderItem} />
+      }
+      <View style={[styles.shadowbox, { width: "100%", padding: 10 }]}>
+        <FlatList data={hasfilter === true ? filterCourse:courseData} renderItem={renderItem} />
       </View>
 
     </View>
@@ -171,29 +158,29 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor : '#fff'
+    backgroundColor: '#fff'
   },
   gridItem: {
     flex: 1,
     marginBottom: 10,
-    paddingTop:5,
+    paddingTop: 5,
     height: 170,
   },
-  txtImg:{
+  txtImg: {
     flex: 1,
     //fontFamily: 'Kanit_400Regular', //เอาไว้ค่อย import มาใหม่
-    left : 20,
-    top : 10,
+    left: 20,
+    top: 10,
     justifyContent: "center",
     alignSelf: "center",
-    fontSize:32,
-    fontWeight : '700',
-    color:"#000",
-    position : 'absolute',
+    fontSize: 32,
+    fontWeight: '700',
+    color: "#000",
+    position: 'absolute',
     // textShadowColor:'#585858',
     // textShadowOffset:{width: 5, height: 5},
     // textShadowRadius:10,
-    },
+  },
   shadowbox: {
     flex: 1,
     borderRadius: 10,
@@ -208,8 +195,8 @@ const styles = StyleSheet.create({
   bgImage: {
     width: "100%",
     height: 150,
-    marginTop : -5,
-    borderRadius : 3,
+    marginTop: -5,
+    borderRadius: 3,
   },
   title: {
     fontSize: 20,
--- screens/DrawerContent.js
+++ screens/DrawerContent.js
@@ -8,98 +8,168 @@ import { TextInput } from "react-native-gesture-handler";
 export const DrawerContent = (props) => {
     const [account, setAccount] = useState(false)
     const [userData, setUserData] = useState("")
-
+    const [height, setHeight] = useState('')
+    const [weight, setWeight] = useState('')
+    const [bmi, setBmi] = useState()
+    const [txtResult, setTxtResult] = useState("")
 
     const signOutAcc = () => {
         signOut(auth)
-        .then(() => {
-            setAccount(true)
-            props.navigation.navigate("Login")
-        })
-        .catch((err) => {
-            console.log(err.message)
-            setAccount(false)
-        })
+            .then(() => {
+                setAccount(true)
+                props.navigation.navigate("Login")
+            })
+            .catch((err) => {
+                console.log(err.message)
+                setAccount(false)
+            })
     }
     onAuthStateChanged(auth, (user) => {
-        if(user){
+        if (user) {
             // console.log(user)
             setUserData(user)
             setAccount(true)
         }
-        else{
+        else {
             // console.log("when logout user value = " +user)
             setAccount(false)
         }
     })
+    const bmiCal = () => {
+        // setFilter(false)
+        setBmi(parseFloat(weight / (height / 100) ** 2).toFixed(2));
+        let result = parseFloat(weight / (height / 100) ** 2).toFixed(2);
+        if (result < 18.5) {
+            setTxtResult("Underweight");
+        } else if (result >= 18.5 && result < 25) {
+            setTxtResult("Normal weight");
+        } else if (result >= 25 && result < 30) {
+            setTxtResult("Over weight");
+        } else if (result >= 30) {
+            setTxtResult("Obese");
+        } else {
+            alert("ใส่ค่าไม่ถูกต้อง!");
+            setTxtResult("กรุณาใส่ค่าใหม่");
+        }
+
+    }
     return (
         <View style={{
-            flex : 1,
-            paddingTop : 30,
+            flex: 1,
+            paddingTop: 30,
         }}>
             {!account ?
-            <View style={{paddingLeft : 20, marginTop : 20}}>
-                <View> 
-                    <TouchableOpacity onPress={() => {props.navigation.navigate("Login")}} style={{flexDirection : 'row', marginBottom :20}}>
-                        <Ionicons name="ios-log-in-outline" size={25} color={'#444444'} style={{marginTop : -2, marginRight : 10, marginLeft : -5}}/>
-                        <Text style={{fontWeight : '600'}}>เข้าสู่ระบบ</Text>
-                    </TouchableOpacity>
-                </View>
-
-                <View>
-                    <TouchableOpacity onPress={() => {props.navigation.navigate("SignUp")}} style={{flexDirection : 'row'}}>
-                        <Ionicons name="ios-person-add-outline" size={20} color={'#444444'} style={{marginTop : -2, marginRight : 10, marginLeft : -1}}/>
-                        <Text style={{fontWeight : '600'}}>ลงทะเบียน</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
-            : 
-            //if had a account
-            <View>
-                <View style={{width : "100%", paddingLeft : 30, paddingTop : 20, marginBottom : 20}}>
-                    <Text style={{fontSize : 40, fontWeight : '600'}}>โปรไฟล์</Text>
-
-                    <View style={{flexDirection : "row"}}>
-                        <Text style={{fontSize : 18, fontWeight : '700', color : '#464646', marginTop :10}}>@{userData.displayName}</Text>
+                <View style={{ paddingLeft: 20, marginTop: 20 }}>
+                    <View>
+                        <TouchableOpacity onPress={() => { props.navigation.navigate("Login") }} style={{ flexDirection: 'row', marginBottom: 20 }}>
+                            <Ionicons name="ios-log-in-outline" size={25} color={'#444444'} style={{ marginTop: -2, marginRight: 10, marginLeft: -5 }} />
+                            <Text style={{ fontWeight: '600' }}>เข้าสู่ระบบ</Text>
+                        </TouchableOpacity>
                     </View>
-                    {/* <View style={{flexDirection : 'row'}}>
+
+                    <View>
+                        <TouchableOpacity onPress={() => { props.navigation.navigate("SignUp") }} style={{ flexDirection: 'row' }}>
+                            <Ionicons name="ios-person-add-outline" size={20} color={'#444444'} style={{ marginTop: -2, marginRight: 10, marginLeft: -1 }} />
+                            <Text style={{ fontWeight: '600' }}>ลงทะเบียน</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                :
+                //if had a account
+                <View>
+                    <View style={{ width: "100%", paddingLeft: 30, paddingTop: 20, marginBottom: 20 }}>
+                        <Text style={{ fontSize: 40, fontWeight: '600' }}>โปรไฟล์</Text>
+
+                        <View style={{ flexDirection: "row" }}>
+                            <Text style={{ fontSize: 18, fontWeight: '700', color: '#464646', marginTop: 10 }}>@{userData.displayName}</Text>
+                        </View>
+                        {/* <View style={{flexDirection : 'row'}}>
                         <Text style={{fontSize : 16, fontWeight : '500', color : '#717171', marginTop :15}}>H : {account.height} cm</Text>
                         <Text style={{fontSize : 16, fontWeight : '500', color : '#717171', marginTop :15, marginLeft : 15}}>W : {account.weight} kg</Text>
                     </View>
                     <Text style={{fontSize : 16, fontWeight : '500', color : '#717171', marginTop :15}}>BMI : {(account.weight / (Math.pow(account.height/100, 2))).toFixed(2)}</Text> */}
-                </View>
-                {/* Line */}
-                <View style={{width : '100%', borderBottomWidth : 1,
-                        borderBottomColor : 'lightgray'}} />
+                    </View>
+                    {/* Line */}
+                    <View style={{
+                        width: '100%', borderBottomWidth: 1,
+                        borderBottomColor: 'lightgray'
+                    }} />
 
 
-                <View style={{width : "100%", paddingLeft : 30, paddingTop : 20}}>
-                    <View> 
-                        <TouchableOpacity onPress={() => {props.navigation.navigate("Home")}} 
-                            style={{marginBottom : 20, flexDirection : 'row'}}>
-                            <Ionicons name="ios-home-outline" size={20} color={'#444444'} style={{marginTop : -2, marginRight : 10}}/>
-                            <Text style={{fontWeight : '600'}}>หน้าหลัก</Text>
+                    <View style={{ width: "100%", paddingLeft: 30, paddingTop: 20 }}>
+                        <View>
+                            <TouchableOpacity onPress={() => { props.navigation.navigate("Home") }}
+                                style={{ marginBottom: 20, flexDirection: 'row' }}>
+                                <Ionicons name="ios-home-outline" size={20} color={'#444444'} style={{ marginTop: -2, marginRight: 10 }} />
+                                <Text style={{ fontWeight: '600' }}>หน้าหลัก</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+
+                    {/* Line */}
+                    <View style={{
+                        width: '100%', borderBottomWidth: 1,
+                        borderBottomColor: 'lightgray'
+                    }} />
+
+
+                    <View style={{ width: '100%', paddingLeft: 30, paddingTop: 20 }}>
+                        <TouchableOpacity onPress={() => { signOutAcc() }} style={{ flexDirection: 'row' }}>
+                            <Ionicons name="ios-exit-outline" size={23} color={'#444444'} style={{ marginTop: -2, marginRight: 10 }} />
+                            <Text style={{ fontWeight: '600' }}>ออกจากระบบ</Text>
                         </TouchableOpacity>
                     </View>
-                </View>
-
-                {/* Line */}
-                <View style={{width : '100%', borderBottomWidth : 1,
-                        borderBottomColor : 'lightgray'}} />
-
-
-                <View style={{width : '100%', paddingLeft : 30, paddingTop : 20}}>
-                    <TouchableOpacity onPress={() => {signOutAcc()}} style={{flexDirection : 'row'}}>
-                        <Ionicons name="ios-exit-outline" size={23} color={'#444444'} style={{marginTop : -2, marginRight : 10}}/>
-                        <Text style={{fontWeight : '600'}}>ออกจากระบบ</Text>
-                    </TouchableOpacity>
-                </View>
-
-                {/* Line */}
-                <View style={{width : '100%', borderBottomWidth : 1,
-                            borderBottomColor : 'lightgray', marginTop : 20}} />
+                    {/* Line */}
+                    <View style={{
+                        width: '100%', borderBottomWidth: 1,
+                        borderBottomColor: 'lightgray', marginTop: 20
+                    }} />
+                    <View style={{ flexDirection: 'row', marginTop: 10, paddingLeft: 30 }}>
+                        <View style={{ width: '40%', marginRight: 10 }}>
+                            <Text style={{ marginBottom: 5 }}>ส่วนสูง</Text>
+                            <TextInput style={{
+                                width: '100%',
+                                height: 40,
+                                borderWidth: 1,
+                                borderColor: 'lightgray',
+                                borderRadius: 5, padding: 5
+                            }}
+                                placeholder="ส่วนสูง"
+                                keyboardType='numeric'
+                                onChangeText={setHeight}
+                                value={height}
+                            />
+                        </View>
+                        <View style={{ width: '40%' }}>
+                            <Text style={{ marginBottom: 5 }}>น้ำหนัก</Text>
+                            <TextInput style={{
+                                width: '100%',
+                                height: 40,
+                                borderWidth: 1,
+                                borderColor: 'lightgray',
+                                borderRadius: 5, padding: 5
+                            }}
+                                placeholder="น้ำหนัก"
+                                keyboardType='numeric'
+                                onChangeText={setWeight}
+                                value={weight}
+                            />
+                        </View>
+                    </View>
+                    <View style={{ alignItems:"center"}}>
+                        <TouchableOpacity style={{
+                            width: '40%', height: 40, backgroundColor: 'lightblue',
+                            borderRadius: 5, alignItems: 'center', justifyContent: 'center', marginTop: 10
+                        }} onPress={() => { bmiCal() }}>
+                            <Text style={{ color: 'white' }}>คํานวณ</Text>
+                        </TouchableOpacity>
+                    <View style={{alignItems:"center"}}>
+                        <Text style={{fontSize:50}}>{bmi}</Text>
+                        <Text style={{fontSize:30}}>{txtResult}</Text>
+                    </View>
+                    </View>
                 </View>
             }
-        </View>
+        </View >
     )
 }
